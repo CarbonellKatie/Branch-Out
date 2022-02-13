5041fb990fee3041dd66728fde5ed515cf7f4ba7
emilyshaw1 <78164386+emilyshaw1@users.noreply.github.com>
2022-02-12 18:48:56 -0600

    Better Mood

--- branch-out/src/Mood/Mood.jsx
+++ branch-out/src/Mood/Mood.jsx
@@ -3,28 +3,33 @@ const Mood = () => {
   return (
     <div id="mood-screen">
         <div id ="body-mood">
-            <table>
-                <tr>
-                    <td><img scr=""></img> </td>
-                    <td> <img scr=""></img></td>
-                    <td> <img scr=""></img></td>
-                    <td> <img scr=""></img></td>
-                    <td> <img scr=""></img></td>
-                    <td> <img scr=""></img></td>
-                    <td> <img scr=""></img></td>
-                </tr>
+            <div id="mood-of-the-day">
                 <table>
                     <tr>
-                        <td><button id = "happy"><img scr = ""></img></button></td>
-                        <td><button id = "sad"><img scr = ""></img></button></td>
-                        <td><button id = "excited"><img scr = ""></img></button></td>
-                        <td><button id = "angry"><img scr = ""></img></button></td>
-                        <td><button id = "lonely"><img scr = ""></img></button></td>
-                        <td><button id = "sociable"><img scr = ""></img></button></td>
-                        <td><button id = "calm"><img scr = ""></img></button></td>
+                        <td><img scr=""></img> </td>
+                        <td> <img scr=""></img></td>
+                        <td> <img scr=""></img></td>
+                        <td> <img scr=""></img></td>
+                        <td> <img scr=""></img></td>
+                        <td> <img scr=""></img></td>
+                        <td> <img scr=""></img></td>
                     </tr>
-                </table>
-            </table>
+                </table>    
+            
+            <div id="options">
+                        <table>
+                            <tr>
+                                <td><button id = "happy"><img scr = ""></img></button></td>
+                                <td><button id = "sad"><img scr = ""></img></button></td>
+                                <td><button id = "excited"><img scr = ""></img></button></td>
+                                <td><button id = "angry"><img scr = ""></img></button></td>
+                                <td><button id = "lonely"><img scr = ""></img></button></td>
+                                <td><button id = "sociable"><img scr = ""></img></button></td>
+                                <td><button id = "calm"><img scr = ""></img></button></td>
+                            </tr>
+                        </table>
+                    </div>
+            </div>
         </div>
     </div>  
     
