d039bcf7c7d7c3ef35de04b9bc4738cc86cf8712
emilyshaw1 <78164386+emilyshaw1@users.noreply.github.com>
2022-02-13 00:29:25 -0600

    background/logo updates

--- branch-out/src/TaskScreen/TaskScreen.jsx
+++ branch-out/src/TaskScreen/TaskScreen.jsx
@@ -1,6 +1,16 @@
 import "./TaskScreen.css";
+import { useState } from "react";
 
 const TaskScreen = ({ showMainScreen }) => {
+  const [input, setState] = useState("");
+  const addRow = () => {
+    const input = document.querySelector("#usertext");
+    console.log(input);
+    const row = document.createElement("tr");
+    row.innerHTML = input;
+    document.querySelector("#task-table").appendChild(row);
+  };
+
   return (
     <div id="main-screen">
       <button id="tasks" onClick={showMainScreen}>
@@ -10,11 +20,13 @@ const TaskScreen = ({ showMainScreen }) => {
         <table id="task-table">
           <tbody>
             <tr>
-              <input
-                type="checkbox"
-                id="Go to the gym"
-                name="Go to the gym"
-              ></input>
+              <td>
+                <input
+                  type="checkbox"
+                  id="Go to the gym"
+                  name="Go to the gym"
+                ></input>
+              </td>
               <td>Go to the gym</td>
               {/* <label for = "Go to the gym">Go to the gym</label> */}
               {/* <td>Go To Gym</td> */}
@@ -30,13 +42,24 @@ const TaskScreen = ({ showMainScreen }) => {
               <td>Talk to someone you don't know</td>
             </tr>
             <tr>
-              <input
-                type="checkbox"
-                id="social event"
-                name="social event"
-              ></input>
+              <td>
+                <input
+                  type="checkbox"
+                  id="social event"
+                  name="social event"
+                ></input>
+              </td>
               <td>Go to a social event</td>
               {/* <label for = "social event">Go to a social event</label> */}
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                <input id="usertext" type="text" />
+                <button onClick={addRow}>Add row</button>
+              </td>
             </tr>
           </tbody>
         </table>
