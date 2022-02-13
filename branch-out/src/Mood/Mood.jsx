855b7ce347bbf26288da14642307c88abda52832
emilyshaw1 <78164386+emilyshaw1@users.noreply.github.com>
2022-02-12 18:32:28 -0600

    Mood body

--- branch-out/src/Mood/Mood.jsx
+++ branch-out/src/Mood/Mood.jsx
@@ -2,10 +2,31 @@ import "./Mood.css";
 const Mood = () => {
   return (
     <div id="mood-screen">
-             <div id ="body-mood">
-            <button id="current-mood"></button>
-            </div>
-     </div>  
+        <div id ="body-mood">
+            <table>
+                <tr>
+                    <td><img scr=""></img> </td>
+                    <td> <img scr=""></img></td>
+                    <td> <img scr=""></img></td>
+                    <td> <img scr=""></img></td>
+                    <td> <img scr=""></img></td>
+                    <td> <img scr=""></img></td>
+                    <td> <img scr=""></img></td>
+                </tr>
+                <table>
+                    <tr>
+                        <td><button id = "happy"><img scr = ""></img></button></td>
+                        <td><button id = "sad"><img scr = ""></img></button></td>
+                        <td><button id = "excited"><img scr = ""></img></button></td>
+                        <td><button id = "angry"><img scr = ""></img></button></td>
+                        <td><button id = "lonely"><img scr = ""></img></button></td>
+                        <td><button id = "sociable"><img scr = ""></img></button></td>
+                        <td><button id = "calm"><img scr = ""></img></button></td>
+                    </tr>
+                </table>
+            </table>
+        </div>
+    </div>  
     
   );
 };
