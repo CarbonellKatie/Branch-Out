3cc98ce072d128ff33a9d0baf599b044a6c29f53
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2022-02-13 04:05:51 -0600

    main page working

--- branch-out/src/MainScreen/MainScreen.jsx
+++ branch-out/src/MainScreen/MainScreen.jsx
@@ -1,6 +1,11 @@
 import "./MainScreen.css";
 import Mood from "../Mood/Mood.jsx";
 import { useEffect } from "react";
+import q4 from "./backgrounds/q4.gif";
+import q1 from "./backgrounds/q1.gif";
+import q2 from "./backgrounds/q2.gif";
+import q3 from "./backgrounds/q3.gif";
+import q5 from "./backgrounds/q5.gif";
 
 const MainScreen = ({ showMoodScreen, showTaskScreen, currMood, tally }) => {
   const imagesMain = [
@@ -12,10 +17,36 @@ const MainScreen = ({ showMoodScreen, showTaskScreen, currMood, tally }) => {
     "./MoodImages/sad.gif",
     "./MoodImages/Socialibe.gif",
   ];
+  var bg = q1;
+  if (tally == 1) {
+    bg = q1;
+  } else if (tally == 2) {
+    bg = q2;
+  } else if (tally == 3) {
+    bg = q3;
+  } else if (tally == 4) {
+    bg = q4;
+  } else if (tally == 5) {
+    bg = q5;
+  }
+
+  var sectionStyle = {
+    width: "100vw",
+    height: "100vh",
+    backgrounSize: "100% 100%",
+    backgroundImage: `url(${bg})`,
+  };
+
+  // useEffect(() => {
+  //   document.querySelector("#body").style.background =
+  //     "url('./backgrounds/q1.gif')";
+  //   document.querySelector("#body").style.backgroundSize = "100% 100%";
+  //   console.log("background change");
+  // }, []);
 
   return (
     <div id="main-screen">
-      <div id="body">
+      <div id="body" style={sectionStyle}>
         <img id="logo" src={require("./logo.png")}></img>
         <div id="tally">{tally}</div>
         {/* <img id="current-state" src={require(`${currMood}`)} /> */}
