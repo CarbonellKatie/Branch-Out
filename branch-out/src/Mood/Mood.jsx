e4e82b020422206d52880812c8bd71e695f7204e
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2022-02-12 22:41:01 -0600

    display image now changes when moon gif clicked

--- branch-out/src/Mood/Mood.jsx
+++ branch-out/src/Mood/Mood.jsx
@@ -1,37 +1,83 @@
 import "./Mood.css";
+import { useState } from "react";
+
 const Mood = ({ showMainScreen }) => {
+  const [state, setState] = useState({ currImage: "./MoodImages/calm.gif" });
+
+  const changeImg = (e) => {
+    // console.log(e.target.id);
+    setState({ currImage: images[parseInt(e.target.id) - 1] });
+  };
+  const images = [
+    "./MoodImages/happy.gif",
+    "./MoodImages/calm.gif",
+    "./MoodImages/excited.gif",
+    "./MoodImages/angry.gif",
+    "./MoodImages/lonely.gif",
+    "./MoodImages/sad.gif",
+    "./MoodImages/Socialibe.gif",
+  ];
+
   return (
     <div id="mood-screen">
       <div id="body-mood">
         <button id="tasks" onClick={showMainScreen}>
           Back to Main
         </button>
-        <img id="current-state" src={require("./MoodImages/calm.gif")} />
+        <img id="current-state" src={require(`${state.currImage}`)} />
         <div id="mood-of-the-day">
           <div id="options">
             <table id="options-table">
               <tbody>
                 <tr>
                   <td>
-                    <img id="img" src={require("./poof.gif")} />
+                    <img
+                      id="1"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/happy.gif")}
+                    />
                   </td>
                   <td>
-                    <img id="img" src={require("./MoodImages/calm.gif")} />
+                    <img
+                      id="2"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/calm.gif")}
+                    />
                   </td>
                   <td>
-                    <img id="img" src={require("./MoodImages/excited.gif")} />
+                    <img
+                      id="3"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/excited.gif")}
+                    />
                   </td>
                   <td>
-                    <img id="img" src={require("./MoodImages/angry.gif")} />
+                    <img
+                      id="4"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/angry.gif")}
+                    />
                   </td>
                   <td>
-                    <img id="img" src={require("./MoodImages/lonely.gif")} />
+                    <img
+                      id="5"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/lonely.gif")}
+                    />
                   </td>
                   <td>
-                    <img id="img" src={require("./MoodImages/sad.gif")} />
+                    <img
+                      id="6"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/sad.gif")}
+                    />
                   </td>
                   <td>
-                    <img id="img" src={require("./MoodImages/Socialibe.gif")} />
+                    <img
+                      id="7"
+                      onClick={(e) => changeImg(e)}
+                      src={require("./MoodImages/Socialibe.gif")}
+                    />
                   </td>
                 </tr>
               </tbody>
