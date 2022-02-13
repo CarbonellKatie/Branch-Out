3d415f323d30ae3173867a2abb1b26438ac2135e
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2022-02-12 22:09:37 -0600

    add back button on task screen

--- branch-out/src/TaskScreen/TaskScreen.jsx
+++ branch-out/src/TaskScreen/TaskScreen.jsx
@@ -1,26 +1,41 @@
 import "./TaskScreen.css";
 
-const TaskScreen = () => {
+const TaskScreen = ({ showMainScreen }) => {
   return (
     <div id="main-screen">
+      <button id="tasks" onClick={showMainScreen}>
+        Back to Main
+      </button>
       <div id="body-task-screen">
         <table id="task-table">
           <tbody>
             <tr>
-              <input type = "checkbox" id = "Go to the gym" name = "Go to the gym"></input>
+              <input
+                type="checkbox"
+                id="Go to the gym"
+                name="Go to the gym"
+              ></input>
               <td>Go to the gym</td>
               {/* <label for = "Go to the gym">Go to the gym</label> */}
               {/* <td>Go To Gym</td> */}
             </tr>
-            <tr> 
+            <tr>
               <td>
-            <input type = "checkbox" id = "Talk to someone you don't know" name = "Talk to someone you don't know"></input>
-            </td>
+                <input
+                  type="checkbox"
+                  id="Talk to someone you don't know"
+                  name="Talk to someone you don't know"
+                ></input>
+              </td>
               <td>Talk to someone you don't know</td>
             </tr>
             <tr>
-            <input type = "checkbox" id = "social event" name = "social event"></input>
-            <td>Go to a social event</td>
+              <input
+                type="checkbox"
+                id="social event"
+                name="social event"
+              ></input>
+              <td>Go to a social event</td>
               {/* <label for = "social event">Go to a social event</label> */}
             </tr>
           </tbody>
